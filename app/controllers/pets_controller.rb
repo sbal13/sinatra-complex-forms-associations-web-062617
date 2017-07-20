dd8a93a4c7f2870531b6f1a122c3da102f4f55db
sbal13 <sbal13@gmail.com>
2017-07-20 00:47:26 -0400

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,18 +1,80 @@
+require 'pry'
 class PetsController < ApplicationController
 
-  get '/pets' do
-    @pets = Pet.all
+  # get '/pets' do
+  #   @pets = Pet.all
+  #   erb :'/pets/index' 
+  # end
+
+  # get '/pets/new' do 
+  #   @owners = Owner.all
+  #   erb :'/pets/new'
+  # end
+
+  # post '/pets' do 
+  #   @pet = Pet.create(params[:pet])
+
+  #   if params[:pet][:owner_id].blank?
+  #     @pet.owner = Owner.create(params[:owner])
+  #   end
+
+  #   redirect to "pets/#{@pet.id}"
+  # end
+
+  # get '/pets/:id/edit' do 
+  #   @pet = Pet.find(params[:id])
+  #   @owner = @pet.owner
+  #   @owners = Owner.all
+  #   erb :'/pets/edit'
+  # end
+
+  # get '/pets/:id' do 
+  #   @pet = Pet.find(params[:id])
+  #   @pet.owner.nil? ? @owner_name = "No owner" : @owner_name = @pet.owner.name
+  #   erb :'/pets/show'
+  # end
+
+  # post '/pets/:id' do 
+
+  #   @pet = Pet.find(params[:id])
+  #   @pet.update(params[:pet])
+
+  #   if !params[:owner].blank?
+  #     @pet.owner = Owner.create(params[:owner])
+  #   end
+
+  #   redirect to "pets/#{@pet.id}"
+  # end
+
+
+   get '/pets' do
+    @pets = Pets.all
     erb :'/pets/index' 
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
 
-    redirect to "pets/#{@pet.id}"
+  post '/pets' do 
+    @pet = Pet.create(params[:pet])
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(params["owner"])
+    end
+
+    @pet.save
+    redirect "/pets/#{@pet.id}"
   end
+
+  get '/pets/:id/edit' do 
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
+  end
+
+
 
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
@@ -21,6 +83,13 @@ class PetsController < ApplicationController
 
   post '/pets/:id' do 
 
-    redirect to "pets/#{@pet.id}"
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(params["owner"])
+    end
+
+    @pet.save
+    redirect to "pets/#{@pet.id}" 
   end
 end
--- spec/controllers/pets_controller_spec.rb
+++ spec/controllers/pets_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'pry'
 
 describe "Pets Controller" do
   describe "new action" do
@@ -91,7 +92,7 @@ describe "Pets Controller" do
     it "edit's the pet's owner with an existing owner" do
       @adam = Owner.create(:name => "Adam")
       visit "/pets/#{@pet.id}/edit"
-      choose(@adam.id)
+      check(@adam.id)
       click_button "Update Pet"
       expect(Pet.last.owner.name).to eq("Adam")
     end
